894d205ea42fae8082716a103003d773d57db0f4
Satish Kumar Pallapu <satishkumarpallapu.16@gmail.com>
2025-10-24 22:05:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4019364476-63a56",
+  "appId": "1:306893902804:web:65e32a5f8f4b3391aa7c41",
+  "apiKey": "AIzaSyCJ-wHdusIfpECW-Z7AglNMX8CorRQkXtk",
+  "authDomain": "studio-4019364476-63a56.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "306893902804"
+};
